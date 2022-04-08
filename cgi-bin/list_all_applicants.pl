d18c0724a36f108698582d52bf276ecd73aced33
Aaron-WSL <aaronbreault@gmail.com>
2022-04-08 11:44:15 -0700

    added edit button

--- cgi-bin/list_all_applicants.pl
+++ cgi-bin/list_all_applicants.pl
@@ -9,40 +9,26 @@ my $db_pw = 'quakeradmin';
 my $db_admiss = 'admissions';
 my $db_table = 'admiss';
 my $dbh = DBI->connect("dbi:mysql:$db_admiss", $db_username, $db_pw);
-my $query = "SELECT first_name, last_name FROM $db_table";
+my $query = "SELECT posting_id, first_name, last_name FROM $db_table";
 #statement handle object
 my $sth = $dbh->prepare($query);
 $sth->execute();
 #array of 2 deep arrays
 my @student_apps;
 while( my $row = $sth->fetchrow_hashref ){
-	push(@student_apps, [ $row->{first_name}, $row->{last_name} ]);
+	push(@student_apps, [ $row->{posting_id}, $row->{first_name}, $row->{last_name} ]);
 }
 my $appslist_html = "<p>first name | last name</p>";
 foreach my $app_row (@student_apps){
-	$appslist_html .= "<br><p>@$app_row[0] | @$app_row[1]</p>";
+	#$appslist_html .= "<br><p>@$app_row[0] | @$app_row[1]</p>";
+	$appslist_html .= "<p>@$app_row[1] | @$app_row[2] ";
+	$appslist_html .= "<button type='button' id='edit_@$app_row[0]' onclick='edit_applicant()'>Edit</button></p>";
 }
 #my $html_line = "<p> this is a line of html programmically inserted </p>";
 
 #qq is the same as double quotes: ""
 my $html_template = qq{
-<!DOCTYPE HTML>
-<html>
-<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
-<meta content="utf-8" http-equiv="encoding">
-<body>
-<h1>Welcome to Phoenix Friend School Admissions</h1>
-<h2>perl test</h2>
 $appslist_html
-<br>
-
-</body>
-<script>
-
-</script>
-
-</html>
-
 };
 
 print $html_template;
