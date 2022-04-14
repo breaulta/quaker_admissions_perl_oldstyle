daaf8bacecab3e30acd2a1138cdc009b7816478c
Aaron-WSL <aaronbreault@gmail.com>
2022-04-13 20:58:46 -0700

    now correctly checks for duplicate username

--- cgi-bin/gen_admin.pl
+++ cgi-bin/gen_admin.pl
@@ -30,7 +30,7 @@ my $db_username = 'test';
 my $db_pw = 'quakeradmin';
 my $db_admiss = 'admissions';
 my $db_table_auth = 'auth';
-my $dbh = DBI->connect("dbi:mysql:$db_table_auth", $db_username, $db_pw);
+my $dbh = DBI->connect("dbi:mysql:$db_admiss", $db_username, $db_pw);
 my $query = "SELECT username FROM $db_table_auth";
 my $sth = $dbh->prepare($query);
 $sth->execute();
@@ -39,19 +39,18 @@ $sth->execute();
 $err = "<p>Username is already taken! Pick another please</p>";
 $html = qq{$err};
 while( my $row = $sth->fetchrow_hashref ){
-	if( $username eq $row ){
+	if( $username eq $row->{username} ){
 		print $html;
 		exit 0;
 	}
 }
 
-
-
-#$dbh->do("INSERT INTO $db_table (first_name, last_name) VALUES (?, ?)", undef, $fname, $lname);
-
-
 my $concat = $password . $salt;
 my $doublesha = sha256_hex($concat);
+
+# enter valid data into table
+# add cookie in a sec
+$dbh->do("INSERT INTO $db_table_auth (username, salt, password_hash) VALUES (?, ?, ?)", undef, $username, $salt, $doublesha);
 
 my $filename = 'auth_log.txt';
 open(my $fh, '>>', $filename) or die "Could not open file '$filename' $!";
@@ -60,8 +59,6 @@ print $fh "\nsalt: $salt";
 print $fh "\npassword: $password";
 print $fh "\ndouble sha: $doublesha";
 close $fh;
-
-#my $q = new CGI;
 
 my $html_line = "<p>You have successfully applied.</p>";
 
